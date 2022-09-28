1dc0ed788ef20d34292228edeafa04cd3490b627
Matt Dies <matthewkdies@gmail.com>
2022-09-27 23:11:42 -0400

    Moving VPN hostname to secrets file.

--- mattdies/check_vpn.py
+++ mattdies/check_vpn.py
@@ -1,7 +1,14 @@
 """Checks the connection of the machine to the Mullvad VPN."""
 
 import json
+import logging
 import subprocess
+from pathlib import Path
+
+from gen_funcs import path_parents  # pylint: disable=import-error
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class NotConnectedError(BaseException):
@@ -20,6 +27,17 @@ def get_mullvad_status_json() -> dict:
     return json.loads(result_stdout)
 
 
+def get_vpn_hostname() -> str:
+    """Gets the VPN hostname from a secrets file.
+
+    Returns:
+        str: The VPN hostname.
+    """
+    vpn_hostname_path = path_parents(Path(__file__))[1] / "secrets/vpn_hostname"
+    with vpn_hostname_path.open("r",encoding="utf-8") as infile:
+        return infile.read()
+
+
 def check_mullvad_status() -> bool:
     """Checks the Mullvad VPN connection status.
 
@@ -30,12 +48,13 @@ def check_mullvad_status() -> bool:
         bool: A boolean representing whether the Mullvad VPN is connected.
     """
     mullvad_status = get_mullvad_status_json()
+    vpn_hostname = get_vpn_hostname()
     exit_ip = bool(mullvad_status["mullvad_exit_ip"])
-    exit_ip_hostname = mullvad_status["mullvad_exit_ip_hostname"] == "ch5-wireguard"
+    exit_ip_hostname = mullvad_status["mullvad_exit_ip_hostname"] == vpn_hostname
     connected = exit_ip and exit_ip_hostname
     if not connected:
         raise NotConnectedError("Not connected to the Mullvad VPN!")
-    print(f"VPN connected! IP address: {mullvad_status['ip']}")
+    logger.info("VPN connected! IP address: %s", mullvad_status["ip"])
     return connected
 
 
