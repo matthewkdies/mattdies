528330e3e2ea51716f3bdea6f28f23c72f2138ec
matthewkdies <74341536+matthewkdies@users.noreply.github.com>
2024-01-08 15:47:48 -0500

    Removing old secrets login from Pi.

--- src/mattdies/gen_funcs.py
+++ src/mattdies/gen_funcs.py
@@ -13,16 +13,4 @@ def path_parents(path: Path) -> list[Path]:
         list[Path]: A list of the path object's parents.
     """
     return list(path.parents)
-
-
-def get_login() -> tuple[str, str]:
-    """Gets system user and pwd.
-
-    Returns:
-        tuple[str, str]: System user and pwd.
-    """
-    with Path("secrets/system").open("r",encoding="utf-8") as system_infile:
-        lines = [line.strip() for line in system_infile.readlines()]
-    user, pwd = lines[0], lines[1]
-    return user, pwd
     
